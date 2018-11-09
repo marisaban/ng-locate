fe16ec4c61f1c326bf25343863d11f6e03ebd05a
Maria Isabel Bances <marisaban7@gmail.com>
2018-11-09 15:44:24 -0800

    Halifax: added bootstrap via npm

---
+++ src/app/list-banks/list-banks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-banks',
+  templateUrl: './list-banks.component.html',
+  styleUrls: ['./list-banks.component.css']
+})
+export class ListBanksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
