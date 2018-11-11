52bf5b9537e5eb0faa483eced0dea0658fb0f7c3
Maria Isabel Bances <marisaban7@gmail.com>
2018-11-11 10:40:34 -0800

    working on template issue

--- src/app/list-branches/list-branches.component.ts
+++ src/app/list-branches/list-branches.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { BranchListService } from '../service/branch-list.service';
-
-// for testing
+// use branch-api directly instead of branch-list
+// import { BranchListService } from '../service/branch-list.service';
 import { BranchAPIService } from '../service/branch-api.service';
+
 import { Branches } from '../branches'
+
+import { Observable } from 'rxjs';
+import {catchError, map, tap, take} from 'rxjs/operators';
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-list-branches',
   templateUrl: './list-branches.component.html',
   styleUrls: ['./list-branches.component.css'],
-  providers: [BranchListService]
+  providers: [BranchAPIService]
 })
 export class ListBranchesComponent implements OnInit {
 
+  private url: string = 'https://api.halifax.co.uk/open-banking/v2.2/branches';
   branches: Branches[] = [];
 
-  constructor(private branchListService: BranchListService,
-              private branchAPIService: BranchAPIService,
+  constructor(private branchAPIService: BranchAPIService,
               private httpClient: HttpClient) { }
 
   public isExpanded: boolean;
 
   ngOnInit() {
+    this.branchAPIService.getBranches()
+    .subscribe((branches: Branches[]) => {
+      this.branches = branches;
+      console.log('branches!!!! ', this.branches);
+    })
+
   }
 
   toggleView(){
@@ -31,3 +41,5 @@ export class ListBranchesComponent implements OnInit {
   }
 
 }
+
+
--- src/app/service/branch-api.service.ts
+++ src/app/service/branch-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {catchError, map, take} from 'rxjs/operators';
+import {catchError, map, tap, take} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
 import { Branches } from '../branches';
@@ -11,16 +11,13 @@ import { Branches } from '../branches';
   providedIn: 'root'
 })
 export class BranchAPIService {
-  // private url: string = 'https://api.halifax.co.uk/open-banking/v2.2/branches';
-  private url: string = 'https://api.github.com/users/marisaban';
+  private url: string = 'https://api.halifax.co.uk/open-banking/v2.2/branches';
   branches: Branches[] = [];
 
   constructor(private httpClient: HttpClient) { }
-  // getBranches(): Observable<Branches[]> {
-  //   return this.httpClient.get<Branches[]>(this.url); 
-  // }
+    // return the api can manipulate it in my component ts file
+    getBranches(): Observable<Branches[]> {
+      return this.httpClient.get<Branches[]>(this.url); 
+    }
 
-  getBranches() {
-    return this.httpClient.get(this.url);
-  }
 }
