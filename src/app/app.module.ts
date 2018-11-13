dd2ba12c55abc64276c199a8290d4745a54708c2
Maria Isabel Bances <marisaban7@gmail.com>
2018-11-13 12:47:53 -0800

    created loop where I match user input to value in the array. Now have to present that information to the user when typed in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BranchAPIService } from './service/branch-api.service';
 import { BranchListService } from './service/branch-list.service';
@@ -26,6 +26,7 @@ import { CityNamePipe } from './pipe/city-name.pipe';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     NgxPaginationModule,
     AgmCoreModule.forRoot({
--- src/app/list-branches/list-branches.component.ts
+++ src/app/list-branches/list-branches.component.ts
@@ -4,6 +4,8 @@ import { BranchAPIService } from '../service/branch-api.service';
 
 import { CityNamePipe } from '../pipe/city-name.pipe';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 interface myData {
   data: any,
   brand: any,
@@ -24,9 +26,19 @@ export class ListBranchesComponent implements OnInit {
   p: number = 1;
   hideme = [];
 
+  cities = [];
+  searchTerms = [];
+
   isLoading: boolean = true;
 
-  constructor(private branchAPIService: BranchAPIService, public cityNamePipe: CityNamePipe) { }
+  // userinput should be saved to this variable 
+  // or the search ts file should have a variable and if it isn't '' then it should be called into this 
+  searchTerm: any = [];
+  userInput: string;
+
+  formVar: FormGroup;
+
+  constructor(private branchAPIService: BranchAPIService, public cityNamePipe: CityNamePipe, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.branchAPIService.getData()
@@ -37,6 +49,33 @@ export class ListBranchesComponent implements OnInit {
         this.records = this.assignRecord(record);
       })
     })
+
+    this.formVar = this.fb.group({
+      searchterm: '',
+    });
+  }
+
+  getDataFromAPI(searchTerm) {
+    let userInput =  searchTerm[0].searchterm;
+    this.branchAPIService.getData()
+    .subscribe(data => {
+      this.isLoading = false;
+      const branchObj = data.data[0].Brand[0].Branch;
+      branchObj.forEach(record => {
+        console.log(searchTerm[0].searchterm, record.Name);
+        if(userInput == record.Name) {
+          console.log("it matches");
+        } else {
+          console.log("nah");
+        }
+      })
+    })
+  }
+
+  onSubmit() {
+    this.searchTerm.push(this.formVar.value);
+    // take object and insert in searchTerm array 
+    this.getDataFromAPI(this.searchTerm);
   }
 
   assignRecord(record) {
@@ -44,6 +83,11 @@ export class ListBranchesComponent implements OnInit {
     this.branches.push(record._recordName);
     return this.branches;
   }
+
+  // matchCity(record) {
+  //   record._cityName = record.Name;
+  //   this.cities.push(record._cityName);
+  // }
 }
 
   
